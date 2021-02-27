0a546da228d0ccda3e3d8bc84c0bd5916f70a048
new-marty <yumabuchi1998@gmail.com>
2021-02-27 11:55:09 +0900

    add calculate function

--- App.java
+++ App.java
@@ -13,5 +13,53 @@ public class App {
 
   public static int calculate(String formula) {
     // TODO
+    String num = "0123456789";
+    String op = "+-";
+    boolean canNext = false;
+    char nextOp = '+';
+    int total = 0;
+    int n = 0;
+    char[] charArr = formula.toCharArray();
+    for (int i = 0; i < charArr.length; i++) {
+      char c = charArr[i];
+      if (op.contains(String.valueOf(c))) {
+        if (!canNext) {
+          System.out.println("invalid input");
+          System.exit(1);
+        }
+        switch (nextOp) {
+          case '+':
+            total += n;
+            break;
+          case '-':
+            total -= n;
+            break;
+        }
+        canNext = false;
+        nextOp = c;
+        n = 0;
+      } else if (num.contains(String.valueOf(c))) {
+        if (!canNext && (int) c == 0) {
+          System.out.println("invalid input");
+          System.exit(1);
+        }
+        n = n * 10 + Character.getNumericValue(c);
+        canNext = true;
+      } else {
+        System.out.println("invalid input");
+        System.exit(1);
+      }
+    }
+    if (canNext) {
+      switch (nextOp) {
+        case '+':
+          total += n;
+          break;
+        case '-':
+          total -= n;
+          break;
+      }
+    }
+    return total;
   }
 }
